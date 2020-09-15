43e3e7bad1dc7cede122fb237aac03c04ea4d86e
luzybry94 <luzybry94@gmail.com>
2020-09-15 14:23:25 -0400

    set up files and environment

---
+++ lib/environment.rb
@@ -0,0 +1,4 @@
+require "nfl_games/version"
+require "nfl_games/cli"
+require "nfl_games/scraper"
+
--- lib/nfl_games.rb
+++
@@ -1,6 +0,0 @@
-require "nfl_games/version"
-
-module NflGames
-  class Error < StandardError; end
-  # Your code goes here...
-end
